18ada400ee18a8eaf91eabd1d96f302ec29172fd
Sai Marn Pha <saimarnpha@mtu.edu.mm>
2023-03-06 22:32:46 +0630

    Seperate head of the page as a component

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,13 @@ import Footer from '../components/footer/Footer';
 import Introduction from '../components/intro/Introduction';
 import Project from '../components/project/Project';
 import Skill from '../components/skill/Skill';
-import Head from 'next/head';
+import Header from '../components/header';
 
 function Home() {
   return (
     <>
+      <Header/>
       <div className='pt-5'></div>
-      <Head>
-
-        <title>Sai Marn Pha</title>
-        <meta name="description" content="Sai Marn Pha portfolio | saimarnpha.com" />
-        <meta name="keywords" content="Sai Marn Pha. Sai Marn Pha portfolio" />
-        <meta name="author" content="Sai Marn Pha" />
-        <link rel="icon" type="image/x-icon" sizes="16x16" href="/favicon.ico" />
-      </Head>
       <Introduction/>
       <About/>
       <Skill />
