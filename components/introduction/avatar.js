b0b4240ff1a4270ed86b491f9628d89fcb559acb
Sai Marn Pha <saimarnpha@mtu.edu.mm>
2023-03-07 22:22:06 +0630

    Fix intro image

--- components/introduction/avatar.js
+++ components/introduction/avatar.js
@@ -6,7 +6,7 @@ export default function Avatar() {
   return (
     <div className='col-sm-12 col-md-6 '>
         <div className='avatar'>
-            <Image className='img-fluid' src={IntroImg} width={200} height={200} 
+            <Image className='img-fluid' src={IntroImg} width={200}
             placeholder="blur" alt="Sai Marn Pha avatar" priority />
         </div>
   </div>
