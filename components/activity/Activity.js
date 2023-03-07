ec7b26f762316e428ac5473b5715cd1bdb996f5f
Sai Marn Pha <saimarnpha@mtu.edu.mm>
2023-03-07 15:37:15 +0630

    Fix Activity section images

--- components/activity/Activity.js
+++ components/activity/Activity.js
@@ -5,7 +5,10 @@ export default function Activity({image, label}) {
   return (
     <div className='col-12 col-sm-12 col-md-6 col-lg-4 '>
         <div className='activity-box border shadow'>
-            <Image src={image} className="img-fluid activity-box-img" placeholder='blur' fill
+            <Image src={image} className="img-fluid activity-box-img" placeholder='blur'
+            sizes="(max-width: 768px) 100vw,
+            (max-width: 1200px) 50vw,
+            33vw"
             alt='Activity at previous at institution' />
             <div className='activity-box-footer'>
                 <div className='p-2'>
