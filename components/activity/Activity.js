a4d8c10b53fee40c3c20f835dbdc224d9d42f8c9
Sai Marn Pha <saimarnpha@mtu.edu.mm>
2023-03-11 11:19:41 +0630

    Refactor for readability - activity

--- components/activity/Activity.js
+++ components/activity/Activity.js
@@ -5,11 +5,13 @@ export default function Activity({image, label}) {
   return (
     <div className='col-12 col-sm-12 col-md-6 col-lg-4 '>
         <div className='activity-box border shadow'>
-            <Image src={image} className="img-fluid activity-box-img" placeholder='blur'
+            <Image src={image} className="img-fluid activity-box-img" 
+            placeholder='blur'
             sizes="(max-width: 768px) 100vw,
             (max-width: 1200px) 50vw,
             33vw"
-            alt='Activity at previous at institution' />
+            alt='Activity at previous at institutions' 
+            />
             <div className='activity-box-footer'>
                 <div className='p-2'>
                     <p className='m-0 para-text-dark'>{label}</p>
